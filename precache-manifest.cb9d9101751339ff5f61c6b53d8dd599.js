badc15a9f7ebcb47d2c659117012dffa1c0cecd8
Oleksandr Tkach <a.tkach@soft-industry.com>
2019-07-04 16:37:57 +0300

    Updates

---
+++ precache-manifest.cb9d9101751339ff5f61c6b53d8dd599.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9eb8fce2bc3f970a4bb94ab345c1dce",
+    "url": "/Geriana/index.html"
+  },
+  {
+    "revision": "76497c79e0ce1a707ea1",
+    "url": "/Geriana/static/css/main.99e8bc79.chunk.css"
+  },
+  {
+    "revision": "6bea2a1750d5e71fa6f7",
+    "url": "/Geriana/static/js/2.91120321.chunk.js"
+  },
+  {
+    "revision": "76497c79e0ce1a707ea1",
+    "url": "/Geriana/static/js/main.85c20374.chunk.js"
+  },
+  {
+    "revision": "37197cb91b2134976c1d",
+    "url": "/Geriana/static/js/runtime~main.c594e038.js"
+  }
+]);
